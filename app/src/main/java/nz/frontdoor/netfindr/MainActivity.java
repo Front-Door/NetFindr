8114ccae81bc72885482228d5f71d2dcd0263f64
Daniel Braithwaite <danielbraithwt@localhost.localdomain>
2016-04-30 11:42:09 +1200

    Add basic background service

--- app/src/main/java/nz/frontdoor/netfindr/MainActivity.java
+++ app/src/main/java/nz/frontdoor/netfindr/MainActivity.java
@@ -1,13 +1,31 @@
 package nz.frontdoor.netfindr;
 
+import android.app.IntentService;
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Intent wifiServiceIntent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        wifiServiceIntent = new Intent(this, WifiService.class);
+        wifiServiceIntent.setData(Uri.parse("START"));
+
+        Button hack = (Button) findViewById(R.id.hack);
+        hack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MainActivity.this.startService(wifiServiceIntent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/nz/frontdoor/netfindr/WifiService.java
@@ -0,0 +1,33 @@
+package nz.frontdoor.netfindr;
+
+import android.app.IntentService;
+import android.content.Intent;
+import android.content.Context;
+import android.net.wifi.WifiManager;
+import android.util.Log;
+import android.widget.Toast;
+
+/**
+ * An {@link IntentService} subclass for handling asynchronous task requests in
+ * a service on a separate handler thread.
+ * <p/>
+ * TODO: Customize class - update intent actions, extra parameters and static
+ * helper methods.
+ */
+public class WifiService extends IntentService {
+    private static final String TAG = "WIFISERVICE";
+
+    public WifiService() {
+        super("WifiService");
+
+        Log.v(TAG, "Service Created");
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        if (intent != null) {
+            final String action = intent.getAction();
+            Log.v(TAG, "Action Recived: " + action);
+        }
+    }
+}
