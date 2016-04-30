9f37d44fd50fde1ed47a83c04f2aaf3405290c7d
Daniel Braithwaite <danielbraithwt@localhost.localdomain>
2016-04-30 18:03:03 +1200

    Create thread lock

--- app/src/main/java/nz/frontdoor/netfindr/ThreadEvent.java
+++ app/src/main/java/nz/frontdoor/netfindr/ThreadEvent.java
@@ -1,7 +1,22 @@
 package nz.frontdoor.netfindr;
 
+import java.util.Objects;
+
 /**
  * Created by danielbraithwt on 4/30/16.
  */
 public class ThreadEvent {
+    private final Object lock = new Object();
+
+    public void signal() {
+        synchronized (lock) {
+            lock.notify();
+        }
+    }
+
+    public void await() throws InterruptedException {
+        synchronized (lock) {
+            lock.wait();
+        }
+    }
 }
