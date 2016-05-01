359bb70629473262a94a6c174afa685a5c9b3a02
Jack Bannister-Sutton <jackbannistersutton@gmail.com>
2016-05-01 12:57:07 +1200

    Updated ConnectionList to display password instead of lat/long, reformated the passing of data to the connection addapter

--- app/src/main/java/nz/frontdoor/netfindr/ConnectionAdapter.java
+++ app/src/main/java/nz/frontdoor/netfindr/ConnectionAdapter.java
@@ -4,6 +4,7 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import java.text.SimpleDateFormat;
@@ -18,33 +19,40 @@ import nz.frontdoor.netfindr.services.Network;
  */
 public class ConnectionAdapter extends RecyclerView.Adapter<ConnectionAdapter.ViewHolder> {
     Database db;
+    private OnNetWorkClickedListner callbackListner;
     SimpleDateFormat SD = new SimpleDateFormat("KK:mm a dd/MM/yy ");
     private List<Network> datain;
 
-    public ConnectionAdapter(Database db){
+    public ConnectionAdapter(Database db,OnNetWorkClickedListner callbackListner){
         this.db = db;
+        this.callbackListner = callbackListner;
         this.datain = this.db.getSuccessfulNetworks();
+
 
     }
 
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         LayoutInflater inflated = LayoutInflater.from(parent.getContext());
-
-
-
-        return new ViewHolder(inflated.inflate(R.layout.connection_item,parent, false));
+        return new ViewHolder(inflated.inflate(R.layout.connection_item, parent, false));
     }
 
     @Override
     public void onBindViewHolder(ViewHolder holder, int position) {
-        Network C = datain.get(position);
-
+        final Network C = datain.get(position);
         holder.data0.setText(SD.format(C.getTimestamp()));
         holder.data1.setText(String.valueOf(C.getWifiName()));
         holder.data2.setText(String.valueOf(C.getSecurityType()));
         holder.data3.setText(String.valueOf(C.getPassword(db).getPhrase()));
         //holder.data3.setText(String.valueOf(C.getLatitude())+"N "+String.valueOf(C.getLongitude())+"E");
+
+        holder.ConnectionItem.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                callbackListner.onNetWorkClicked(C);
+            }
+        });
+
     }
 
     @Override
@@ -54,14 +62,20 @@ public class ConnectionAdapter extends RecyclerView.Adapter<ConnectionAdapter.Vi
 
     public static class ViewHolder extends RecyclerView.ViewHolder {
 
-        public TextView data0,data1,data2,data3,data4;
+        public TextView data0,data1,data2,data3;
+        public LinearLayout ConnectionItem;
 
         public ViewHolder(View itemView) {
             super(itemView);
+            ConnectionItem = (LinearLayout) itemView.findViewById(R.id.connection_item);
             data0 = (TextView) itemView.findViewById(R.id.time_data0);
             data1 = (TextView) itemView.findViewById(R.id.time_data1);
             data2 = (TextView) itemView.findViewById(R.id.time_data2);
             data3 = (TextView) itemView.findViewById(R.id.time_data3);
         }
+    }
+    interface OnNetWorkClickedListner{
+        void onNetWorkClicked(Network netWork);
+
     }
 }
